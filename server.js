0c61f0d0ae6a3f0230b7179d6108c6abe5d22c71
Jesse Martin <jesse.martin@ensighten.com>
2012-09-12 13:05:22 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express'),
-  server = express.createServer(),
+  server = express(),
   fs = require('fs'),
   jadeBundler = require('jadeBundler');
 
 // Compile specific views
 jadeBundler.bundle(process.cwd() + '/templates');
 
-// Compile general views
-// jadeBundler.compile(process.cwd() + '/node_modules/ensPrivacyUserInterface/public/templates');
+// Compile other views
+// jadeBundler.compile(process.cwd() + '/node_modules/other_view_directory');
 
 // Define and export the views
 jadeBundler.exportJson('./public/js/allViews.js');
